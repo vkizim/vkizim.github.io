ab1ec92c86439317e20f91dcef04d0ba74eeefa3
vkizim <vkizim@gmail.com>
2021-11-21 11:05:14 +0200

    add doker color icon in doc folder

---
+++ docs/precache-manifest.9c5b0f05d8aad905ac05ee9a9edfe1a3.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1275902bc56fc4c0a68164803fc5a23c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a7b4955a384bc0f5325",
+    "url": "/static/css/3.21ea103e.chunk.css"
+  },
+  {
+    "revision": "56855d4a2d55c50f71dd",
+    "url": "/static/css/main.f3b44dc5.chunk.css"
+  },
+  {
+    "revision": "6e6cf14d568713192d59",
+    "url": "/static/js/2.8a6c5863.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.8a6c5863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a7b4955a384bc0f5325",
+    "url": "/static/js/3.253a448f.chunk.js"
+  },
+  {
+    "revision": "56855d4a2d55c50f71dd",
+    "url": "/static/js/main.1b434513.chunk.js"
+  },
+  {
+    "revision": "5383ef26fc6613bf52c3",
+    "url": "/static/js/runtime-main.732ba4ca.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "f1df2a516c4c815cfe46e5b54620d7fa",
+    "url": "/static/media/dkrs.f1df2a51.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "b521546a0bb20a0a002094cc4e62bd5d",
+    "url": "/static/media/shevchenko.b521546a.jfif"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "b546572e317ea5cb81178b118ec70818",
+    "url": "/static/media/ubi.b546572e.jfif"
+  }
+]);
